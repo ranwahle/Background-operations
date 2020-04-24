0ce8a83e9724343956bb15a60200ccc2adcc4315
ranwahle <ran.wahle@gmail.com>
2020-04-24 11:11:32 +0300

    Adding https support for prod

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ echo.on('connection', function(conn) {
 });
 const isProd = !!process.env.PORT;
 app.get('*', (req, res, next) => {
+    console.log(req.headers);
     if (isProd && req.protocol === 'http') {
         res.redirect('https://' + req.headers.host + req.url);
     } else {
